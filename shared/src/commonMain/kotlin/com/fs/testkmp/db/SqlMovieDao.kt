c77bf1d4fbc8f65fd319f271a5ba5bb03e868528
Nail Shaykhraziev <nail.shaykhraziev@flatstack.com>
2021-12-07 11:17:53 +0300

    Add database queries

--- shared/src/commonMain/kotlin/com/fs/testkmp/db/SqlMovieDao.kt
+++ shared/src/commonMain/kotlin/com/fs/testkmp/db/SqlMovieDao.kt
@@ -2,23 +2,50 @@ package com.fs.testkmp.db
 
 import com.fs.testkmp.data.Movie
 import com.fs.testkmp.data.MovieResponse
+import com.fs.testkmp.database.AppDatabase
+import comfstestkmp.MovieQueries
 
-class SqlMovieDao() : MovieDao {
+class SqlMovieDao(
+    private val database: AppDatabase
+) : MovieDao {
 
     override suspend fun saveMovie(movieEntity: MovieResponse) {
-        TODO("Not yet implemented")
+        database.movieQueries.run {
+            transaction {
+                insertMovie(movieEntity)
+            }
+        }
     }
 
     override suspend fun saveAll(movieEntities: List<MovieResponse>) {
-        TODO("Not yet implemented")
+        database.movieQueries.run {
+            transaction {
+                movieEntities.forEach { movieEntity ->
+                    insertMovie(movieEntity)
+                }
+            }
+        }
+    }
+
+    private fun MovieQueries.insertMovie(movieEntity: MovieResponse) {
+        insertMovies(
+            movieEntity.id.toLong(),
+            movieEntity.title,
+            movieEntity.voteCount?.toLong(),
+            movieEntity.voteAverage,
+            movieEntity.posterPath,
+            movieEntity.releaseDate,
+            movieEntity.overview,
+            movieEntity.genres
+        )
     }
 
     override suspend fun getAll(): List<Movie> {
-        TODO("Not yet implemented")
+        return database.movieQueries.selectMovies(::mapMovie).executeAsList()
     }
 
     override suspend fun getMovieById(id: Int): Movie? {
-        TODO("Not yet implemented")
+        return database.movieQueries.selectMovieById(id.toLong(), ::mapMovie).executeAsOneOrNull()
     }
 
     override suspend fun deleteById(id: Int) {
@@ -26,8 +53,28 @@ class SqlMovieDao() : MovieDao {
     }
 
     override suspend fun deleteAll() {
-        TODO("Not yet implemented")
+        database.movieQueries.deleteAll()
     }
+
+    private fun mapMovie(
+        id: Long,
+        title: String,
+        vote_count: Long?,
+        vote_average: Double?,
+        poster_path: String,
+        release_date: String,
+        overview: String,
+        genre_ids: List<Int>?
+    ): Movie = Movie(
+        id = id.toInt(),
+        title = title,
+        voteCount = vote_count?.toInt(),
+        voteAverage = vote_average,
+        posterPath = poster_path,
+        releaseDate = release_date,
+        overview = overview,
+        genres = genre_ids
+    )
 
     companion object {
 //        const val
--- shared/src/commonMain/kotlin/com/fs/testkmp/di/dbModule.kt
+++ shared/src/commonMain/kotlin/com/fs/testkmp/di/dbModule.kt
@@ -1,15 +1,19 @@
 package com.fs.testkmp.di
 
 import com.fs.testkmp.data.MovieEntity
+import com.fs.testkmp.database.AppDatabase
 import com.fs.testkmp.db.KodeinMovieDao
 import com.fs.testkmp.db.MovieDao
 import com.fs.testkmp.db.SqlMovieDao
-import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.ColumnAdapter
+import comfstestkmp.Movies
 import org.kodein.db.DB
 import org.kodein.db.impl.open
 import org.kodein.db.orm.kotlinx.KotlinxSerializer
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+
+const val FILE_PATH_QUALIFIER = "FilePath"
 
 val dbModule = module {
 
@@ -19,14 +23,28 @@ val dbModule = module {
         })
     }
 
+    single {
+        AppDatabase.invoke(driver = get(), moviesAdapter = Movies.Adapter(listOfIntAdapter))
+    }
+
     single<MovieDao>(named("Kodein")) {
         KodeinMovieDao(get())
     }
 
     single<MovieDao>(named("SqlDelight")) {
-        SqlMovieDao()
+        SqlMovieDao(get())
     }
-
 }
 
-const val FILE_PATH_QUALIFIER = "FilePath"
+private val listOfIntAdapter = object : ColumnAdapter<List<Int>, String> {
+    override fun decode(databaseValue: String) =
+        if (databaseValue.isEmpty()) {
+            listOf()
+        } else {
+            databaseValue.split(",").map {
+                it.toInt()
+            }
+        }
+
+    override fun encode(value: List<Int>) = value.joinToString(separator = ",")
+}
